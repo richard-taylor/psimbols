12d4ee5a74c0f6e11ed208bccced12537ff27341
Richard Taylor <rt@richardtaylor.co.uk>
2018-07-04 20:07:50 +0100

    Added 3 test clients.

--- psimbols/register.py
+++ psimbols/register.py
@@ -1,11 +1,14 @@
 
 import psimbols.client
 import psimbols.err
-    
+
+ID = '123'
+KEY = b'ABCDEFGHIJKLMNOPQRSTUVWX'
+
 class Register:
 
     def get_client(self, id):
 
         if id == '123':
-            return psimbols.client.Client('123', 'localhost:9393', '789')
+            return psimbols.client.Client(ID, 'localhost:9393', KEY)
         return None
